cad049eb4589df62026a5df6e3718a9b6b42dcca
Sifytul <sifytul09@gmail.com>
2022-12-27 13:12:40 +0600

    Object project completed

---
+++ object-project/test/02-values-in-object.js
@@ -0,0 +1,40 @@
+// const assert = require("assert");
+
+// const valuesInObject = require("../problems/02-values-in-object.js");
+
+// describe("valuesInObject()", function() {
+//     it("should return an array containing all object values", function() {
+//             const animals = valuesInObject({
+//             dog: "Wolfie",
+//             cat: "Jet",
+//             bison: "Bilbo"
+//         });
+//             const animalResult = ["Wolfie", "Jet", "Bilbo"];
+
+//             const foods = valuesInObject({
+//             apple: "tart",
+//             lemon: "sour",
+//             mango: "sweet"
+//         });
+//         const foodResult = ["tart", "sour", "sweet"];
+
+//         assert.deepEqual(animals, animalResult);
+//         assert.deepEqual(foods, foodResult);
+//     });
+// });
+
+const assert = require('assert');
+
+const valuesInObject = require('../problems/02-values-in-object')
+
+describe("valuesInObject()", ()=>{
+    it("should return an array containing all object values", () => {
+        const food = valuesInObject({
+            mango: "sweet", 
+            dragonFruit: "salty",
+            tamarind: "sour"
+        })
+        const foodResult = ["sweet", "salty", "sour"]
+        assert.deepEqual(food, foodResult)
+    })
+})
